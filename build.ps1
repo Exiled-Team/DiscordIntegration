dc012d9d25d7988136a3a88d3c8d8ace69506381
Franco <francoquiroz_13@live.com>
2021-10-08 17:31:52 -0300

    Ah yes no sleep

---
+++ build.ps1
@@ -0,0 +1,49 @@
+#! pwsh
+
+param (
+    [Switch]$BuildNuGet
+)
+
+#cd CerberusProjects
+
+$Projects = @(
+    "DiscordIntegration"
+)
+
+function Execute {
+    param (
+        [string]$Cmd
+    )
+
+    foreach ($Project in $Projects) {
+        Invoke-Expression ([string]::Join(' ', $Cmd, $Project, $args))
+        CheckLastOperationStatus
+    }
+}
+
+function CheckLastOperationStatus {
+    if ($? -eq $false) {
+        Exit 1
+    }
+}
+
+function GetSolutionVersion {
+    [XML]$PropsFile = Get-Content Cerberus.props
+    $Version = $PropsFile.Project.PropertyGroup[2].Version
+    $Version = $Version.'#text'.Trim()
+    return $Version
+}
+
+# Restore projects
+Execute 'dotnet restore'
+# Build projects
+Execute 'dotnet build'
+# Build a NuGet package if needed
+if ($BuildNuGet) {
+    $Version = GetSolutionVersion
+    $Year = [System.DateTime]::Now.ToString('yyyy')
+
+    Write-Host "Generating NuGet package for version $Version"
+
+    nuget pack Exiled/Exiled.nuspec -Version $Version -Properties Year=$Year
+}
