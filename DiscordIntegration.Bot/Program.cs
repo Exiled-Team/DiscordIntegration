106a55eb21fac228ee07523801357c4fef6fbb95
joker-119 <amathor929@gmail.com>
2022-05-19 18:36:51 -0700

    oops

---
+++ DiscordIntegration.Bot/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace DiscordIntegration.Bot;
+
+using Newtonsoft.Json;
+
+public static class Program
+{
+    private static Config? _config;
+    private static string KCfgFile = "DiscordIntegration-config.json";
+    private static List<Bot> _bots = new();
+
+    public static Config Config => _config ??= GetConfig();
+    public static Random Rng { get; } = new();
+
+    public static void Main(string[] args)
+    {
+        if (args.Contains("--debug"))
+            Config.Debug = true;
+        foreach (KeyValuePair<ushort, string> botToken in Config.BotTokens)
+            _bots.Add(new Bot(botToken.Key, botToken.Value));
+        AppDomain.CurrentDomain.ProcessExit += (_, _) =>
+        {
+            foreach (Bot bot in _bots)
+                bot.Destroy();
+        };
+    }
+
+    private static Config GetConfig()
+    {
+        if (File.Exists(KCfgFile))
+            return JsonConvert.DeserializeObject<Config>(File.ReadAllText(KCfgFile))!;
+        File.WriteAllText(KCfgFile, JsonConvert.SerializeObject(Config.Default, Formatting.Indented));
+        return Config.Default;
+    }
+}
