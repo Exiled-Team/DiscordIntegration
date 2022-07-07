9835e1e138c81f08d6dedfaf1e22a359641acbb1
Ficus-x <64978711+Ficus-x@users.noreply.github.com>
2022-07-07 20:22:31 +0300

    Add Get command

--- DiscordIntegration.Bot/Commands/SlashCommands.cs
+++ DiscordIntegration.Bot/Commands/SlashCommands.cs
@@ -44,6 +44,27 @@ public class SendCommand : InteractionModuleBase<SocketInteractionContext>
         }
     }
 
+    [SlashCommand("get", "Shows which commands you can execute.")]
+    public async Task ShowAvailableCommandsAsync([Summary("server", "The server number to get available commands.")] ushort serverNum)
+    {
+        if (!Program.Config.ValidCommands.ContainsKey(serverNum))
+        {
+            await RespondAsync("This server was not found.", ephemeral: true);
+            return;
+        }
+        
+        List<KeyValuePair<ulong, List<string>>> availableCommands = Program.Config.ValidCommands[serverNum]
+            .Where(command => ((IGuildUser) Context.User).Hierarchy >= Context.Guild.GetRole(command.Key).Position).ToList();
+
+        if (!availableCommands.Any())
+        {
+            await RespondAsync("There are no available commands for you.", ephemeral: true);
+            return;
+        }
+        
+        await RespondAsync("You can use these commands:\n" + string.Join("\n", availableCommands), ephemeral: true);
+    }
+    
     public int CanRunCommand(IGuildUser user, ushort serverNum, string command)
     {
         if (!Program.Config.ValidCommands.ContainsKey(serverNum) || Program.Config.ValidCommands[serverNum].Count == 0)
